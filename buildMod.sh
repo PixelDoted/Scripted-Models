0b153e4971ea9802ef66ec2f3e327f00d19932f5
PixelDoted <34966285+PixelDoted@users.noreply.github.com>
2022-06-11 20:51:19 -0500

    version bump

--- buildMod.sh
+++ buildMod.sh
@@ -1,2 +1,2 @@
 ./gradlew build
-cp ./build/libs/scripted-models-0.5.0.jar ~/.minecraft/mods/scripted-models-0.5.0.jar
+cp ./build/libs/scripted-models-0.5.1.jar ~/.minecraft/mods/scripted-models-0.5.1.jar
