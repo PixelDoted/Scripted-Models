250e308321ad03e56bed9423beb52e988d22dc41
PixelDots <isaac@marketplaceplus.com>
2022-08-08 10:41:11 -0500

    use abyte instead of an enum for LineMode

--- src/main/java/me/pixeldots/scriptedmodels/script/line/Line.java
+++ src/main/java/me/pixeldots/scriptedmodels/script/line/Line.java
@@ -5,7 +5,7 @@ public class Line {
     public LineType type;
     public Object[] data;
 
-    public void run(Object extras, LineMode mode) {
+    public void run(Object extras, byte mode) {
         if (type.mode != mode && type.mode != LineMode.GLOBAL) return;
         type.func.run(data, extras);
     }
